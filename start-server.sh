12131a9c073f73b3de5429ca94c8014b3c71003d
Ayo Ogunrekun <ayoola66@gmail.com>
2025-11-22 21:41:00 +0000

    Rename play_index.html to index.html for Netlify auto-serving

--- start-server.sh
+++ start-server.sh
@@ -8,7 +8,7 @@ sleep 1
 
 # Start the server
 echo "Starting HTTP server on port 8000..."
-echo "Access your game at: http://localhost:8000/play_index.html"
+echo "Access your game at: http://localhost:8000/"
 echo "Press Ctrl+C to stop the server"
 python3 -m http.server 8000
 
